cb2afbf028ef841baaf47a3b46038fde7e0aaaa9
DDenysKoval <metalheadden@gmail.com>
2025-05-29 11:07:21 +0000

    Deploying to gh-pages from @ DDenysKoval/BlendedJS6@b54c26cc20a9e4b05a009d54da60fe34bcce888e 🚀

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import"./assets/styles-BK7AYJoX.js";import{a as r}from"./assets/vendor-N5iQpiFS.js";const s={form:document.querySelector(".search-form"),categoriesList:document.querySelector(".categories"),productsList:document.querySelector(".products")},o=12,c=0,n={limit:o,skip:c,orientation:"portrait"};async function a(){try{return await r.get("https://dummyjson.com/products/category-list")}catch(t){console.log(t.message)}}async function i(){try{return await r.get("https://dummyjson.com/products/",{params:n})}catch(t){console.log(t.message)}}function u(t){return t.map(e=>`
-    <li class="categories-item">${e}</li>`).join("")}function m(t){return t.map(e=>`
-    <li class="product-item">
-      <h2 class="product-title">${e.title}</h2>
-      <p class="product-text">${e.brand}</p>
-      <img src="${e.thumbnail}" alt="${e.description}">
-      <p class="product-price">$${e.price}</p>
-    </li>`).join("")}async function p(){try{const t=await a();s.categoriesList.innerHTML=u(t.data)}catch(t){console.log(t.message)}}async function l(t){t.preventDefault();try{const e=await i();s.productsList.innerHTML=m(e.data.products)}catch(e){console.log(e.message)}}document.addEventListener("DOMContentLoaded",p);s.form.addEventListener("submit",l);
+import"./assets/styles-BK7AYJoX.js";import{a as i}from"./assets/vendor-N5iQpiFS.js";const s={form:document.querySelector(".search-form"),categoriesList:document.querySelector(".categories"),productsList:document.querySelector(".products")};async function d(){try{return await i.get("https://dummyjson.com/products/category-list")}catch(t){console.log(t.message)}}async function l(){const t={limit:12,skip:(o-1)*12,orientation:"portrait"};try{return await i.get("https://dummyjson.com/products/",{params:t})}catch(e){console.log(e.message)}}async function p(t){const e={limit:12,skip:(o-1)*12,orientation:"portrait"};try{return await i.get(`https://dummyjson.com/products/category/${t}`,{params:e})}catch(a){console.log(a.message)}}function g(t){return t.unshift("All"),t.map(e=>`
+    <li class="categories__item">
+      <button class="categories__btn" type="button">${e}</button>
+    </li>`).join("")}function n(t){return t.map(e=>`
+    <li class="products__item" data-id="${e.id}">
+      <img class="products__image" src="${e.thumbnail}" alt="${e.description}"/>
+      <p class="products__title">${e.title}</p>
+      <p class="products__brand"><span class="products__brand--bold">Brand: ${e.brand}</span></p>
+      <p class="products__category">Category: ${e.category}  </p>
+      <p class="products__price">Price: ${e.price}$</p>
+    </li>`).join("")}async function m(){try{const t=await d();s.categoriesList.innerHTML=g(t.data)}catch(t){console.log(t.message)}}let o=1,c;async function y(t){t.preventDefault(),o=1;try{const e=await l();s.productsList.innerHTML=n(e.data.products)}catch(e){console.log(e.message)}}async function _(t){if(o=1,document.querySelectorAll(".categories__btn").forEach(r=>r.classList.remove("categories__btn--active")),t.target.nodeName!=="BUTTON")return;c=t.target.textContent,t.target.closest(".categories__btn").classList.add("categories__btn--active");try{const r=await p(c);if(s.productsList.innerHTML=n(r.data.products),c==="All"){const u=await l();s.productsList.innerHTML=n(u.data.products)}}catch(r){console.log(r.message)}}document.addEventListener("DOMContentLoaded",m);s.form.addEventListener("submit",y);s.categoriesList.addEventListener("click",_);
 //# sourceMappingURL=index.js.map
